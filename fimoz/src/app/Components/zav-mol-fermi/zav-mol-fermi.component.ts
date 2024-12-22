f58a47fd052abac4e70f1b16f0fe457339594dc7
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-12-22 18:38:38 +0300

    de

--- fimoz/src/app/Components/zav-mol-fermi/zav-mol-fermi.component.ts
+++ fimoz/src/app/Components/zav-mol-fermi/zav-mol-fermi.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-zav-mol-fermi',
   templateUrl: './zav-mol-fermi.component.html',
-  styleUrls: ['./zav-mol-fermi.component.css']
+  styleUrls: ['./zav-mol-fermi.component.scss']
 })
 export class ZavMolFermiComponent {
   // Здесь можно добавить логику для кнопок и других элементов
