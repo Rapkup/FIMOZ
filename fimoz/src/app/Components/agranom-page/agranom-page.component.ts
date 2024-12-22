e1db8143a3c833561b9260bc4b3411cb768a821a
xderkx12 <derk4327@gmail.com>
2024-12-22 17:37:44 +0300

    add-agranom-pag-routs

---
+++ fimoz/src/app/Components/agranom-page/agranom-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agranom-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './agranom-page.component.html',
+  styleUrl: './agranom-page.component.scss'
+})
+export class AgranomPageComponent {
+
+}
--- fimoz/src/app/app.routes.ts
+++ fimoz/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { AgranomPageComponent } from './Components/agranom-page/agranom-page.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // Главная страница
+  { path: 'agranom-page', component: AgranomPageComponent }, // Главная страница
 ];
