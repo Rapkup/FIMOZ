c9436f1ea1be5da5ae308fda22c7f2d9cbd1e550
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-12-22 18:38:38 +0300

    de

--- fimoz/src/app/app.routes.ts
+++ fimoz/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { AgranomPageComponent } from './Components/agranom-page/agranom-page.component';
-
+import { ZavMolFermiComponent } from './Components/zav-mol-fermi/zav-mol-fermi.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // Главная страница
   { path: 'agranom-page', component: AgranomPageComponent }, // Главная страница
+  { path: 'zav-mol-fermi-page', component: ZavMolFermiComponent }, // Главная страница
 ];
