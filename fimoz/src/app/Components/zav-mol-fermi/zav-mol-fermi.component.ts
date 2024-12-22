a5fef70a4a7fa1a097981b68532f9f6f1f9aecc3
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-12-22 18:23:32 +0300

    e

---
+++ fimoz/src/app/Components/zav-mol-fermi/zav-mol-fermi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zav-mol-fermi',
+  standalone: true,
+  imports: [],
+  templateUrl: './zav-mol-fermi.component.html',
+  styleUrl: './zav-mol-fermi.component.scss'
+})
+export class ZavMolFermiComponent {
+
+}
